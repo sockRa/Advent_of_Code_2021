4c577dffc7b4efdac218091532b9856a58519a31
Jimmy Manninen <manninenjimmy@gmail.com>
2021-12-01 09:01:57 +0100

    Initial

---
+++ Day1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var previousDepthWindow = int.MaxValue;
+            var depthIncrease = 0;
+            var globalCount = 0;
+            
+            var input = File.ReadAllLines(@"../../input.txt");
+            
+            for (var i = 0; i < (input.Length - 2); i++)
+            {
+                var currentDepthWindow = 0;
+
+                for (var x = globalCount; x < (3 + globalCount); x++)
+                {
+                    currentDepthWindow += int.Parse(input[x]);
+                }
+
+                globalCount++;
+                
+                // only occurs once
+                if (previousDepthWindow == int.MaxValue)
+                {
+                    previousDepthWindow = currentDepthWindow;
+                    continue;
+                }
+
+                if (currentDepthWindow > previousDepthWindow)
+                {
+                    depthIncrease++;
+                }
+
+                previousDepthWindow = currentDepthWindow;
+            }
+            
+
+            Console.WriteLine(depthIncrease);
+            Console.ReadKey();
+        }
+    }
+}
