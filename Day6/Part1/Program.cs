4060117e22e74cbda4023cecc243d56a5f9d0eac
Jimmy Manninen <manninenjimmy@gmail.com>
2021-12-06 20:54:56 +0100

    Part1

---
+++ Day6/Part1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Part1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"..\..\input.txt").First().Split(',');
+            var fish = input.Select(timer => new Lanternfish(int.Parse(timer))).ToList();
+
+            Simulate(fish, 80);
+        }
+
+        private static void Simulate(ICollection<Lanternfish> fishes, int days)
+        {
+            var dayCount = 0;
+            
+            while (dayCount != days)
+            {
+                var currentFishStock = new List<Lanternfish>(fishes);
+                
+                foreach (var fish in currentFishStock)
+                {
+                    if (fish.internalTimer != 0)
+                    {
+                        fish.internalTimer--;
+                        continue;
+                    }
+
+                    fish.internalTimer = 6;
+                    fishes.Add(new Lanternfish(8));
+                }
+
+                dayCount++;
+            }
+            
+            Console.WriteLine(fishes.Count);
+        }
+    }
+}
