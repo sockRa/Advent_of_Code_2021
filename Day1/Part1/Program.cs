36ab7c36a4efe42933059af4c0b949406df6b658
Jimmy Manninen <manninenjimmy@gmail.com>
2021-12-06 13:41:12 +0100

    Part 1

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Part1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"../../input.txt");
+
+            var previousNumber = input[0];
+            var currentNumber = 0;
+            var depthIncrease = 0;
+            
+            foreach (var s in input.Skip(1))
+            {
+                if (int.Parse(s) > int.Parse(previousNumber))
+                {
+                    depthIncrease++;
+                }
+
+                previousNumber = s;
+            }
+            
+            Console.WriteLine(depthIncrease);
+        }
+    }
+}
