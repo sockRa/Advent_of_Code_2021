6890dab6f19860228cb39459c74c438fc70d27ac
Jimmy Manninen <manninenjimmy@gmail.com>
2021-12-06 13:44:17 +0100

    Part2

---
+++ Day1/Part2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Part2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var previousDepthWindow = int.MaxValue;
+            var depthIncrease = 0;
+            var globalCount = 0;
+
+            var input = File.ReadAllLines(@"../../input.txt");
+
+            for (var i = 0; i < (input.Length - 2); i++)
+            {
+                var currentDepthWindow = 0;
+
+                for (var x = globalCount; x < (3 + globalCount); x++)
+                {
+                    currentDepthWindow += int.Parse(input[x]);
+                }
+
+                globalCount++;
+
+                // only occurs once
+                if (previousDepthWindow == int.MaxValue)
+                {
+                    previousDepthWindow = currentDepthWindow;
+                    continue;
+                }
+
+                if (currentDepthWindow > previousDepthWindow)
+                {
+                    depthIncrease++;
+                }
+
+                previousDepthWindow = currentDepthWindow;
+            }
+
+
+            Console.WriteLine(depthIncrease);
+        }
+    }
+}
