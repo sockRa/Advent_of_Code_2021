dbe4c0fd52fbac110b0fd44a541f4938a1cbda54
Jimmy Manninen <manninenjimmy@gmail.com>
2021-12-07 10:20:57 +0100

    Part 1

---
+++ Day7/Part1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Part1
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"..\..\input.txt").First().Split(',');
+            var crabPositions = Array.ConvertAll(input, int.Parse);
+            
+            var min = crabPositions.Min();
+            var max = crabPositions.Max();
+
+            var currentFuelCost = 0;
+            var cheapestFuelCost = int.MaxValue;
+
+            for (var i = min; i < max; i++)
+            {
+                foreach (var position in crabPositions)
+                {
+                    currentFuelCost += Math.Abs(position - i);
+                }
+                
+                if (currentFuelCost < cheapestFuelCost)
+                {
+                    cheapestFuelCost = currentFuelCost;
+                }
+
+                currentFuelCost = 0;
+            }
+            
+            Console.WriteLine(cheapestFuelCost);
+        }
+    }
+}
